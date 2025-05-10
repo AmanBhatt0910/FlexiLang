a6f9e55115a1927c2b511f31d2af1a1d054601a9
Aman Bhatt <amanbhatt0910@gmail.com>
2025-05-10 22:59:26 +0530

    Auth routes added

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
-import translateRoutes from './routes/translateRoutes.js';
+// import translateRoutes from './routes/translateRoutes.js';
 
 dotenv.config();
 connectDB();
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
-app.use('api/translate', translateRoutes);
+// app.use('/api/translate', translateRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
