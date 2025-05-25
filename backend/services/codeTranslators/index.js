440c08c7124e4c10bcbcb36f07e23944ab66cebf
Aman Bhatt <amanbhatt0910@gmail.com>
2025-05-25 18:26:50 +0530

    Refined Backend modules

--- backend/services/codeTranslators/index.js
+++ backend/services/codeTranslators/index.js
@@ -1,3 +1,8 @@
 export { jsToPython } from './jsToPython.js';
 export { pythonToJs } from './pythonToJs.js';
+
 export { javaToJs } from './javaToJs.js';
+export { jsToJava } from './jsToJava.js';
+
+export { javaToC } from './javaToC.js';
+export { cToJava } from './cToJava.js';
